c65688d5bbafe5585dda6f361155918285c6fef7
freek <freek@00c1905e-64e4-0310-a44b-c1523ea362d7>
2004-09-18 21:58:37 +0000

    First Import Of 1.1.1
    git-svn-id: file:///opt/REPOS/SVN/schedtool/trunk@1 00c1905e-64e4-0310-a44b-c1523ea362d7

---
+++ error.h
@@ -0,0 +1,21 @@
+/*
+ Schedtool
+ (c) by Freek <email_undisclosed>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation version 2 of the License.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ Please see the file LICENSE for details.
+
+ */
+
+/* include it here, to let the main also use errno */
+#include <errno.h>
+
+void decode_error(char *, ...);
---
+++ syscall_magic.h
@@ -0,0 +1,22 @@
+/* 
+ I don't know where this was exactly taken from, but I think I found it
+ in glibc.
+ */
+#include <linux/unistd.h>
+#define __NR_sched_setaffinity	241
+#define __NR_sched_getaffinity	242
+
+/*
+ a nice macro to define the following:
+ it's a syscall with 3 args,
+ it returns int,
+ it's named sched_....,
+ the next arg is of type pid_t,
+ has the local name pid,
+ next is unsigned int,
+ with name len,
+ then an unsigned long *,
+ named user_mask_ptr
+ */
+_syscall3 (int, sched_setaffinity, pid_t, pid, unsigned int, len, unsigned long *, user_mask_ptr)
+_syscall3 (int, sched_getaffinity, pid_t, pid, unsigned int, len, unsigned long *, user_mask_ptr)
