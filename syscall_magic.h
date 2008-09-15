7c968e7c5078b05c7830305d7e9f393083a71669
freek <freek@00c1905e-64e4-0310-a44b-c1523ea362d7>
2008-09-15 15:40:10 +0200

    Document that syscall_magic.h is obsolete and remove dependecy from Makefile

--- syscall_magic.h
+++ syscall_magic.h
@@ -1,3 +1,10 @@
+/*
+ this sticks around for documentation issues only and will be removed in the next
+ release - it documents the direct syscalls for affinity, without going thru glibc
+ */
+
+#if 0
+
 /* 
  I don't know where this was exactly taken from, but I think I found it
  in glibc.
@@ -20,3 +27,5 @@
  */
 _syscall3 (int, sched_setaffinity, pid_t, pid, unsigned int, len, unsigned long *, user_mask_ptr)
 _syscall3 (int, sched_getaffinity, pid_t, pid, unsigned int, len, unsigned long *, user_mask_ptr)
+
+#endif
